c57f2c4c561a93f2ac064b8b16b78669c9d0785b
olowo <olowodarey8@gmail.com>
2025-07-03 20:56:19 +0100

    json read

---
+++ json_parser/target/debug/deps/json_parser-1a22824b40dac7ad.d
@@ -0,0 +1,5 @@
+/home/olowo/Desktop/rust projects /json_parser/target/debug/deps/libjson_parser-1a22824b40dac7ad.rmeta: src/main.rs
+
+/home/olowo/Desktop/rust projects /json_parser/target/debug/deps/json_parser-1a22824b40dac7ad.d: src/main.rs
+
+src/main.rs:
---
+++ json_parser/target/debug/deps/json_parser-92eeb714de195859.d
@@ -0,0 +1,5 @@
+/home/olowo/Desktop/rust projects /json_parser/target/debug/deps/libjson_parser-92eeb714de195859.rmeta: src/main.rs
+
+/home/olowo/Desktop/rust projects /json_parser/target/debug/deps/json_parser-92eeb714de195859.d: src/main.rs
+
+src/main.rs:
